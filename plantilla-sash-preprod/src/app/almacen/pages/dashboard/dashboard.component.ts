dbbbd0a9628075376b49b5c710084193215f0355
MariaPoala <maria.vilcapoma22@gmail.com>
2024-12-11 22:18:01 -0500

    m

--- plantilla-sash-preprod/src/app/almacen/pages/dashboard/dashboard.component.ts
+++ plantilla-sash-preprod/src/app/almacen/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Subscriber } from 'rxjs';
+import { Observable, Subscriber } from 'rxjs';
 import { UrlEnviromentService } from 'src/app/shared/services/url-enviroment.service';
 
 @Component({
@@ -15,17 +15,31 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
 
   }
-  GetRolesUser() {
-    console.log('llego hasta aqui: ' + this.envUrl.urlAddress + 'users/list/roles-user/');
+  // GetRolesUser() {
+  //   console.log('llego hasta aqui: ' + this.envUrl.urlAddress + 'users/list/roles-user/');
+  //   return this.httpClient.get(this.envUrl.urlAddress + 'users/list/roles-user/');
+  // }
+  GetRolesUser(): Observable<any> {
     return this.httpClient.get(this.envUrl.urlAddress + 'users/list/roles-user/');
   }
   listMenu() {
-    this.GetRolesUser().subscribe(resp => {
-      console.log(resp['success'] + 'listMenu')
-      if (resp['success'] == true) {
-        this.data = resp['data']
+    // this.GetRolesUser().subscribe(resp => {
+    //   console.log(resp['data.id'] + 'listMenu')
+    //   if (resp['success'] == true) {
+    //     this.data = resp['data']
+    //   }
+    // })
+    this.GetRolesUser().subscribe(
+      (response) => {
+        const userId = response.data.id; // Accede a 'id'
+        const userRoles = response.data.rol; // Accede a 'roles'
+        console.log('id:', userId);
+        console.log('Roles:', userRoles);
+      },
+      (error) => {
+        console.error('Error fetching roles:', error);
       }
-    })
+    );
   }
 
 }
